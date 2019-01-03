d9011f7cc5d4b24acbfc20a652103c90cd1098a2
Galvanize Web Development <Galvanize@Isaacs-MacBook-Pro.local>
2019-01-03 15:55:10 -0700

    added classes and some other things

---
+++ src/Components/MessageList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import '../App.css';
+import Message from "./Message.js"
+class MessageList extends Component{
+    render(){
+        return(
+            <Message></Message>
+        )
+    }
+}
+export default MessageList
